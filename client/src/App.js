eade93675296593dfb2a317111b542c44acea2e3
Chris Grigg <chris.grigg@codafication.com>
2021-08-25 16:22:07 +1000

    correct image names

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React AWS ELastic Beanstalk
+            Learn React AWS ELastic Beanstalk - One more change
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
